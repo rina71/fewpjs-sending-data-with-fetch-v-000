c5fa983f28d33e5227511907dd65e43f3a7997a8
Yurina Shin <yurinas@gmail.com>
2020-09-01 02:08:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your code here
+let formData = {
+  userName: "Byron",
+  useEmail: "ser@gmail.com"
+};
+
+
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+
+fetch("http://localhost:3000/users", configObj)
